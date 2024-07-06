9a736d9c8a741f5a0b797f8cf1f9216ac8b3496f
alphaovi <ovi778811@gmail.com>
2024-07-07 02:17:32 +0600

    zod validation for create a product

--- src/app/modules/Product/product.controller.ts
+++ src/app/modules/Product/product.controller.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express';
 import { ProductServices } from '../product.service';
+import { productValidationSchema } from '../product.zod.validation';
 
 const createProduct = async (req: Request, res: Response) => {
   try {
     const product = req.body.product;
 
+    // data validation using zod
+    const zodValidateProduct = productValidationSchema.parse(product);
+
     // will call service function to send this data
-    const result = await ProductServices.createProductIntoDB(product);
+    const result = await ProductServices.createProductIntoDB(zodValidateProduct);
 
     // send response
     res.status(200).json({
---
+++ src/app/modules/product.zod.validation.ts
@@ -0,0 +1,24 @@
+import {z} from "zod";
+
+const VariantValidationSchema = z.object({
+    type: z.string().nonempty("Type is required"),
+    value: z.string().nonempty("Value is required"),
+});
+
+const InventoryValidationSchema = z.object({
+    quantity: z.number(),
+    inStock: z.boolean(),
+});
+
+export const productValidationSchema = z.object({
+    name: z.string().nonempty("Name is requried"),
+    description: z.string().nonempty("Description is requried"),
+    price: z.number(),
+    category: z.string().nonempty("Category is requried"),
+    tags: z.array(z.string().nonempty("Tages is required")),
+    variants: z.array(VariantValidationSchema),
+    inventory: InventoryValidationSchema,
+});
+
+
+
